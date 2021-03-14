45d420604461ad7a7103491bfbb10ed06d96c86d
lisprolog <lisprolog@users.noreply.github.com>
2021-03-14 13:09:06 +0100

    (% comments)

--- disciplines.pl
+++ disciplines.pl
@@ -1,47 +1,47 @@
-/*
-usage:
-swipl -s disciplines.pl
-animalism(X).
-X = nosferatu ;
-X = gangrel ;
-X = tzimisce ;
-X = ravnos.
-*/
+%
+% usage:
+% swipl -s disciplines.pl
+% animalism(X).
+% X = nosferatu ;
+% X = gangrel ;
+% X = tzimisce ;
+% X = ravnos.
+%
 
-(animalism(nosferatu)).
-(animalism(gangrel)).
-(animalism(tzimisce)).
-(animalism(ravnos)).
-(obfuscate(nosferatu)).
-(obfuscate(assamite)).
-(obfuscate(setite)).
-(obfuscate(malkavian)).
-(potence(nosferatu)).
-(potence(brujah)).
-(potence(lasombra)).
-(potence(giovanni)).
-(celerity(brujah)).
-(celerity(toreador)).
-(celerity(assamite)).
-(presence(brujah)).
-(presence(toreador)).
-(presence(ventrue)).
-(presence(setite)).
-(auspex(toreador)).
-(auspex(malkavian)).
-(auspex(tremere)).
-(auspex(tzimisce)).
-(dominate(ventrue)).
-(dominate(tremere)).
-(dominate(giovanni)).
-(dominate(lasombra)).
-(fortitude(ventrue)).
-(fortitude(gangrel)).
-(fortitude(ravnos)).
-(dementation(malkavian)).
-(thaumaturgy(tremere)).
-(necromancy(giovanni)).
-(vicissitude(tzimisce)).
-(chimerstry(ravnos)).
-(serpentis(setite)).
-(quietus(assamite)).
+animalism(nosferatu).
+animalism(gangrel).
+animalism(tzimisce).
+animalism(ravnos).
+obfuscate(nosferatu).
+obfuscate(assamite).
+obfuscate(setite).
+obfuscate(malkavian).
+potence(nosferatu).
+potence(brujah).
+potence(lasombra).
+potence(giovanni).
+celerity(brujah).
+celerity(toreador).
+celerity(assamite).
+presence(brujah).
+presence(toreador).
+presence(ventrue).
+presence(setite).
+auspex(toreador).
+auspex(malkavian).
+auspex(tremere).
+auspex(tzimisce).
+dominate(ventrue).
+dominate(tremere).
+dominate(giovanni).
+dominate(lasombra).
+fortitude(ventrue).
+fortitude(gangrel).
+fortitude(ravnos).
+dementation(malkavian).
+thaumaturgy(tremere).
+necromancy(giovanni).
+vicissitude(tzimisce).
+chimerstry(ravnos).
+serpentis(setite).
+quietus(assamite).
